d1ede0da3f30d5df8e9955481702e307590a58dd
Maksym Zadorozhniuk <9175593@stud.op.edu.ua>
2025-10-15 14:39:20 +0300

    lab 4 complete

---
+++ src/main/java/ua/opnu/DiscountBill.java
@@ -0,0 +1,48 @@
+package ua.opnu;
+
+import ua.opnu.java.inheritance.bill.Employee;
+import ua.opnu.java.inheritance.bill.GroceryBill;
+import ua.opnu.java.inheritance.bill.Item;
+
+public class DiscountBill extends GroceryBill {
+    private int discountCount;
+    private double discountAmount;
+    private final boolean regularCustomer;
+
+    public DiscountBill(Employee clerk, boolean regularCustomer) {
+        super(clerk);
+        this.regularCustomer = regularCustomer;
+        this.discountCount = 0;
+        this.discountAmount = 0.0;
+    }
+
+    @Override
+    public void add(Item i) {
+        super.add(i);
+        if (regularCustomer && i.getDiscount() > 0.0) {
+            discountCount++;
+            discountAmount += i.getDiscount();
+        }
+    }
+
+    @Override
+    public double getTotal() {
+        if (!regularCustomer) {
+            return super.getTotal();
+        } else {
+            return super.getTotal() - discountAmount;
+        }
+    }
+
+    public int getDiscountCount() {
+        return regularCustomer ? discountCount : 0;
+    }
+
+    public double getDiscountAmount() {
+        return regularCustomer ? discountAmount : 0.0;
+    }
+
+    public double getDiscountPercent() {
+        return regularCustomer ? 100 - ((getTotal() * 100) / (super.getTotal())) : 0.0;
+    }
+}
