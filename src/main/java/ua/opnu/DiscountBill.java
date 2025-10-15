75207cb94b92e1697c974ba41d0702a3985d008d
Maksym Zadorozhniuk <9175593@stud.op.edu.ua>
2025-10-15 22:54:54 +0300

    lab 4 complete

--- src/main/java/ua/opnu/DiscountBill.java
+++ src/main/java/ua/opnu/DiscountBill.java
@@ -5,19 +5,20 @@ import ua.opnu.java.inheritance.bill.GroceryBill;
 import ua.opnu.java.inheritance.bill.Item;
 
 public class DiscountBill extends GroceryBill {
+
     private int discountCount;
     private double discountAmount;
     private final boolean regularCustomer;
 
-    public DiscountBill(Employee clerk, boolean regularCustomer) {
+    public DiscountBill(final Employee clerk, final boolean isRegularCustomer) {
         super(clerk);
-        this.regularCustomer = regularCustomer;
+        this.regularCustomer = isRegularCustomer;
         this.discountCount = 0;
         this.discountAmount = 0.0;
     }
 
     @Override
-    public void add(Item i) {
+    public void add(final Item i) {
         super.add(i);
         if (regularCustomer && i.getDiscount() > 0.0) {
             discountCount++;
@@ -35,14 +36,26 @@ public class DiscountBill extends GroceryBill {
     }
 
     public int getDiscountCount() {
-        return regularCustomer ? discountCount : 0;
+        if (regularCustomer) {
+            return discountCount;
+        } else {
+            return 0;
+        }
     }
 
     public double getDiscountAmount() {
-        return regularCustomer ? discountAmount : 0.0;
+        if (regularCustomer) {
+            return discountAmount;
+        } else {
+            return 0.0;
+        }
     }
 
     public double getDiscountPercent() {
-        return regularCustomer ? 100 - ((getTotal() * 100) / (super.getTotal())) : 0.0;
+        if (regularCustomer) {
+            return (discountAmount * 100) / super.getTotal();
+        } else {
+            return 0.0;
+        }
     }
 }
